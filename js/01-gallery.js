a4ae0f2d57a631a138ac1c1ec8604cb6d2575807
Grinevich599 <sgrinevich08@gmail.com>
2023-11-13 12:00:45 +0200

    first commit

---
+++ js/01-gallery.js
@@ -0,0 +1,30 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+ 
+const imageList = document.querySelector(".gallery");
+
+galleryItems.forEach((image) => {
+    const listItem = document.createElement("li");
+    listItem.insertAdjacentHTML(
+      "beforeend",
+      `<a class="gallery__link" href="${image.original}">
+      <img class = "gallery__image" src="${image.preview}" data-source="large-image.jpg" alt="${image.description}" width = "100%" height = "100%"/>
+      </a>`
+    );
+    listItem.classList.add('gallery__item');
+    
+
+    listItem.addEventListener('click', (event) => {
+      event.preventDefault();
+        const lightbox = basicLightbox.create(`
+          <img src="${image.original}" alt="${image.description}" width="100%" height="100%"/>
+        `);
+    
+        lightbox.show();
+      });
+      imageList.appendChild(listItem);
+  });
+
+  
+  
